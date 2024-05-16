87c4c37cfbcbaee95f141fbc1947f104259c19b5
GrandKou <rieub123@gmail.com>
2024-05-16 19:55:53 +0800

    bahala namo ako e commit

--- app/src/main/java/com/example/ecommerce_project1/account_page.java
+++ app/src/main/java/com/example/ecommerce_project1/account_page.java
@@ -1,6 +1,8 @@
 package com.example.ecommerce_project1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class account_page extends AppCompatActivity {
 
+    TextView username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_account_page);
+
+        Intent intent = getIntent();
+        username = findViewById(R.id.username);
+        String user_name = intent.getStringExtra("user_name");
+        username.setText(user_name);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/ecommerce_project1/register_page.java
+++ app/src/main/java/com/example/ecommerce_project1/register_page.java
@@ -3,6 +3,8 @@ package com.example.ecommerce_project1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,6 +18,8 @@ import com.example.ecommerce_project1.R;
 
 public class register_page extends AppCompatActivity {
     TextView linkSignin;
+    EditText username;
+    Button Register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,21 @@ public class register_page extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        username = findViewById(R.id.eUsernameRegister);
+        Register = findViewById(R.id.btnSignin);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Username = username.getText().toString();
+                Intent intent = new Intent(register_page.this, account_page.class);
+                intent.putExtra("user_name", Username);
+                startActivity(intent);
+            }
+        });
+
+
         linkSignin = (TextView) findViewById(R.id.txtSignin);
         linkSignin.setOnClickListener(new View.OnClickListener() {
             @Override
